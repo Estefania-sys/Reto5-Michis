28b1a2d17b1b3636fb8b0a25a378023583aa9778
Pablo Úbeda <pubedac@campusdigitalfp.com>
2026-05-07 13:19:51 +0200

    Cambios en el CSS + Iconos

--- contacto.php
+++ contacto.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Contáctanos</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"> <!-- Incluimos el CSS de Font Awesome -->
 </head>
 <body>
 <?php include 'navbar/header.php'?>
@@ -12,8 +13,8 @@
     <p>Si tienes alguna duda respecto a los animales que tenemos en la protectora, no dudes en contactarnos en:</p>
 </section>
 <section class="botoneracontacto">
-    <p><a class="contactobtn" href="mailto:correo@ejemplo.com">📩 Envíame un Email clicando aquí a correo@ejemplo.com</a></p>
-    <p><a class="contactobtn" href="tel:123456789">📞 Llámanos clicando aquí o al número: 123 45 67 89</a></p>
+    <p><a class="contactobtn" href="mailto:correo@ejemplo.com"><i class="fa-solid fa-envelope-circle-check"></i> Envíame un Email clicando aquí a correo@ejemplo.com</a></p>
+    <p><a class="contactobtn" href="tel:123456789"><i class="fa-solid fa-phone"></i> Llámanos clicando aquí o al número: 123 45 67 89</a></p>
 </section>
 
 <?php include 'navbar/footer.php' ?>
