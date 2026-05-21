d0f5dac5aa97a227a69bd465c2852b367e4ceab5
Pablo Úbeda <pubedac@campusdigitalfp.com>
2026-05-21 13:55:17 +0200

    Comentarios

--- Clases/Cliente.php
+++ Clases/Cliente.php
@@ -1,8 +1,11 @@
 <?php
 // Clase para ADOPTANTES (Sin login)
-class Cliente extends Persona {
+class Cliente extends Persona { 
+  // Persona, clase padre
+  // Cliente, clase que hereda de persona
   public function __construct($id, $nombres, $apellidos, $email) {
     parent::__construct($id, $nombres, $apellidos, $email);
+    // Crea un nuevo  cliente usando el constructor de persona
   }
 }
 ?>
