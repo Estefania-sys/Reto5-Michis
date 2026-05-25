de9fe378f4d8ee9eacb1a71650f6efb78117eb1f
Pablo Úbeda <pubedac@campusdigitalfp.com>
2026-05-25 09:12:42 +0200

    Subida correcciones header admin

--- navbar/footer.php
+++ navbar/footer.php
@@ -2,29 +2,27 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"> </head>
 <footer class="main-footer">
     <section class="container footer-content">
-        <section class="footer-izqda">
+        <section class="footer-left">
             <section class="footer-brand">
                 <section class="logo">
                     <a href="/Reto5-Michis/index.php"><img class="logoimg" src="/Reto5-Michis/Imagenes/Items/logo.png" height="55" width="55"></a><h4>Associació Còlonies Gats Sant Quirze Safaja</h4>
                 </section>
                 <p class="traductor" data-es="Dando una segunda oportunidad a los michis desde 2026." data-ca="Donant una segona oportunitat als michis des de 2026."></p>
             </section>
-        </section>
 
-        <section class="footer-social">
-            &nbsp;
-            <h3 class="traductor" data-es="¡Síguenos en nuestras redes sociales!" data-ca="Segueix-nos a les nostres xarxes socials!"></h3>
-            <section class="social-icons">
-                <a href="#" title="Facebook"><i class="fa-brands fa-square-facebook"></i></a>
-                <a href="#" title="Instagram"><i class="fa-brands fa-square-instagram"></i></a>
-                <a href="#" title="Twitter"><i class="fa-brands fa-square-x-twitter"></i></a>
-            </section>
+            <section class="footer-social">
+                &nbsp;
+                <h3 class="traductor" data-es="¡Síguenos en nuestras redes sociales!" data-ca="Segueix-nos a les nostres xarxes socials!"></h3>
+                <section class="social-icons">
+                    <a href="#" title="Facebook"><i class="fa-brands fa-square-facebook"></i></a>
+                    <a href="#" title="Instagram"><i class="fa-brands fa-square-instagram"></i></a>
+                    <a href="#" title="Twitter"><i class="fa-brands fa-square-x-twitter"></i></a>
+                </section>
         </section>
-    </section>
     
-    <section class="footer-bottom">
-        <section class="container">
-            </section>
+        <section class="mapa">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1115.5486644128396!2d2.1578995959206373!3d41.717116349536234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4c296945e4337%3A0xeb773dc72f429e40!2sCarrer%20del%20Puig%20Dari%2C%2025%2C%2008189%20Sant%20Quirze%20Safaja%2C%20Barcelona!5e0!3m2!1ses!2ses!4v1779450977160!5m2!1ses!2ses" width="300" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </section>
     </section>
 </footer>
 
--- navbar/headeradmin.php
+++ navbar/headeradmin.php
@@ -1,23 +1,23 @@
-<header class="navbar">
+<header class="navbaradmin">
     <section class="logo">
         <a href="/Reto5-Michis/Admin/admin-index.php"><img class="logoimg" src="/Reto5-Michis/Imagenes/Items/logo.png" height="55" width="55" alt="Logo"></a>
     </section>
-    <section class="cambio_idioma">
+    <section class="cambio_idioma_admin">
         <button class="btn-lang" data-lang="es"><img src="/Reto5-Michis/Imagenes/Items/banderaespaña.png" alt="Español" height="35" width="55"></button>
         <button class="btn-lang" data-lang="ca"><img src="/Reto5-Michis/Imagenes/Items/banderacataluna.png" alt="Catalán" height="35" width="55"></button>
     </section>
     <nav>
         <ul>
-            <a href="/Reto5-Michis/index.php" class="traductor" data-es="Inicio" data-ca="Inici"></a>
-            <a href="/Reto5-Michis/catalogo.php" class="traductor" data-es="Adoptar" data-ca="Adoptar"></a>
-            <a href="/Reto5-Michis/Blog/finales.php" class="traductor" data-es="Blog" data-ca="Blog"></a>
-            <a href="/Reto5-Michis/contacto.php" class="traductor" data-es="Contacto" data-ca="Contacte"></a>
-            
+            <li><a href="/Reto5-Michis/index.php" class="traductor" data-es="Inicio" data-ca="Inici"></a></li>
+            <li><a href="/Reto5-Michis/catalogo.php" class="traductor" data-es="Adoptar" data-ca="Adoptar"></a></li>
+            <li><a href="/Reto5-Michis/Blog/finales.php" class="traductor" data-es="Blog" data-ca="Blog"></a></li>
+            <li><a href="/Reto5-Michis/contacto.php" class="traductor" data-es="Contacto" data-ca="Contacte"></a></li>
+
             <?php if (isset($_SESSION['admin']) && !empty($_SESSION['admin'])): ?>
                 <span class="traductor" data-es="Bienvenid@, <?php echo htmlspecialchars($_SESSION['admin']); ?>" data-ca="Benvingut/da, <?php echo htmlspecialchars($_SESSION['admin']); ?>"></span>
-                <a class="logoutbtn traductor" href="/Reto5-Michis/logout.php" data-es="Cerrar Sesión" data-ca="Tancar Sessió"></a>
+                <li><a class="logoutbtn traductor" href="/Reto5-Michis/logout.php" data-es="Cerrar Sesión" data-ca="Tancar Sessió"></a></li>
             <?php else: ?>
-                <a href="/Reto5-Michis/login.php" class="btn-login traductor" data-es="Admin Login" data-ca="Admin Login"></a>
+                <li><a href="/Reto5-Michis/login.php" class="btn-login traductor" data-es="Admin Login" data-ca="Admin Login"></a></li>
             <?php endif; ?>
         </ul>
     </nav>
